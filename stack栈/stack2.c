be8e384e95a62c5e6f7b7623caf2c946d6b392c2
chimewu <wujicheng0215@outlook.com>
2023-10-31 14:57:47 +0800

    添加链式存储的栈

---
+++ stack栈/stack2.c
@@ -0,0 +1,79 @@
+//实现一个栈，要求用链式存储结构，且实现入栈、出栈、取栈顶元素、判断栈空、判断栈满等操作。
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node;
+
+typedef struct stack
+{
+    Node *top;
+    int size;
+} Stack;
+
+void init(Stack *stack)
+{
+    stack->top = NULL;
+    stack->size = 0;
+}
+
+void push(Stack *stack, int data)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    node->next = stack->top;
+    stack->top = node;
+    stack->size++;
+}
+
+int pop(Stack *stack)
+{
+    if (stack->size == 0)
+    {
+        printf("stack is empty\n");
+        return -1;
+    }
+    Node *node = stack->top;
+    int data = node->data;
+    stack->top = node->next;
+    free(node);
+    stack->size--;
+    return data;
+}
+
+int peek(Stack *stack)
+{
+    if (stack->size == 0)
+    {
+        printf("stack is empty\n");
+        return -1;
+    }
+    return stack->top->data;
+}
+
+bool isEmpty(Stack *stack)
+{
+    return stack->size == 0;
+}
+
+bool isFull(Stack *stack)
+{
+    return false;
+}
+
+void print(Stack *stack)
+{
+    Node *node = stack->top;
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
